bd8cc3cc90e894b25744f1a805c3d2f1b0a8a329
phdaccache <pedrodaccache@gmail.com>
2023-04-27 21:11:43 -0300

    changes in program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
