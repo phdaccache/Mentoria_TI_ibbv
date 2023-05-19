ccd56ce413e3791b9a486ce7404ae51c88238780
phdaccache <pedrodaccache@gmail.com>
2023-05-18 21:51:10 -0300

    const

--- ex03.cs
+++ ex03.cs
@@ -8,8 +8,8 @@ namespace Mentoria_TI_ibbv
     public class ex03
     {
         static void Main(string[] args){
+            const double pi = 3.14;
             int raio;
-            double pi = 3.14;
             Console.Write("Raio do circulo: ");
             raio = int.Parse(Console.ReadLine()!);
 
